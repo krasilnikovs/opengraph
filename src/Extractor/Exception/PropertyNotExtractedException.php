071d5a110dd954137d112572fd9b6bf587dd7d40
mihails.krasilnikovs <mihail.krasilnikov.j@gmail.com>
2025-01-25 23:29:29 +0200

    refactor: moved classes and renaming

--- src/Extractor/Exception/PropertyNotExtractedException.php
+++ src/Extractor/Exception/PropertyNotExtractedException.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 namespace Krasilnikovs\Opengraph\Extractor\Exception;
+
 use RuntimeException;
 
 final class PropertyNotExtractedException extends RuntimeException
